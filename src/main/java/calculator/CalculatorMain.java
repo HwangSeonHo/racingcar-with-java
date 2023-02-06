d09325df9eb8e6c60c8d882e649af3cf417c1798
황선호/서비스개발팀/BM <seonho.hwang@bugs.co.kr>
2023-02-06 18:14:14 +0900

    feat: 패키지구조

---
+++ src/main/java/calculator/CalculatorMain.java
@@ -0,0 +1,22 @@
+package calculator;
+
+public class CalculatorMain {
+
+    public static void main(String[] args){
+
+        //show user interface
+
+        //select operation type
+
+        //select input type
+
+        //input
+
+        //calculate
+
+        //make result
+
+        //output
+    }
+
+}
